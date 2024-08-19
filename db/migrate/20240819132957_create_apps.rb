034d1df97bfe60aae1a2a31f776ef46fabbccbdd
kevin peramallee <kevloevinnoah@gmail.com>
2024-08-19 16:11:16 +0200

    models

---
+++ db/migrate/20240819132957_create_apps.rb
@@ -0,0 +1,13 @@
+class CreateApps < ActiveRecord::Migration[7.1]
+  def change
+    create_table :apps do |t|
+      t.string :nom
+      t.date :date_de_prelevement
+      t.date :date_de_renouvellement
+      t.decimal :montant
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
