a572548ef22b3b3c01332fbb7a176444cc216224
kevin peramallee <kevloevinnoah@gmail.com>
2024-08-22 15:15:48 +0200

    form user_app

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get 'new_demarche', to: 'user_apps#new'
   resources :users
   resources :user_apps, only: [:new, :create, :index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
