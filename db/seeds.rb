615edb0664ab7ab407739f4198e1a5b4b98ea654
Christine Basila <kristybasila@gmail.com>
2024-08-23 11:42:02 +0200

    actualisation logo applications user

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,12 @@
 App.destroy_all
 
 p "creating EDF app"
+
+edf.logo = "edf.png"
 edf = App.new
 edf.name = "EDF"
 edf.url_redirections = {
+  addresse: "https://particulier.edf.fr/fr/accueil/autres-pages/demenagement-edf.html?at_platform=google&at_medium=sl&at_campaign=GN_Demenagement_NUL_2024_Classique_NUL&at_creation=144877738243&at_term=changement%20adresse%20%C3%A9lectricit%C3%A9&at_extension=&at_loc=9055710&at_device=c&gad_source=1&gclid=CjwKCAjw5qC2BhB8EiwAvqa41qLMMotWj13xr3rf29N1vYPusxP4hoyo1Ny7oIPDNand0by1f99VTBoCTakQAvD_BwE&gclsrc=aw.ds" ,
   rib: "https://www.edf.fr/entreprises/faq-entreprises/contrat/modifier-votre-contrat/comment-communiquer-un-nouveau-rib-pour-payer-vos-factures-edf-en-ligne"
 }
 
@@ -24,6 +27,8 @@ edf.save!
 
 
 p "creating ENGIE app"
+
+engie.logo = "engie.jpg"
 engie = App.new
 engie.name =	"ENGIE"
 engie.url_redirections = {
@@ -34,6 +39,8 @@ engie.save!
 
 
 p "creating ENI app"
+
+eni.logo = "eni.png"
 eni = App.new
 eni.name = "ENI"
 eni.url_redirections ={
@@ -49,11 +56,12 @@ eni.save!
 
 p "creating Impot app"
 
+impot.logo = "impot.jpg"
 impot_gouv = App.new
 impot_gouv.nom = "Impot_gouv"
 impot_gouv.url_redirections = {
-addresse: "https://www.impots.gouv.fr/particulier/questions/je-viens-de-demenager-quelles-demarches-dois-je-accomplir-aupres-de",
-rib: "https://www.impots.gouv.fr/particulier/questions/comment-modifier-mes-coordonnees-bancaires-mon-rib-pour-payer-mes-impots#:~:text=Pour%20effectuer%20cette%20modification%2C%20rendez,%C3%A0%20jour%20vos%20coordonn%C3%A9es%20bancaires%20%C2%BB."
+ addresse: "https://www.impots.gouv.fr/particulier/questions/je-viens-de-demenager-quelles-demarches-dois-je-accomplir-aupres-de",
+ rib: "https://www.impots.gouv.fr/particulier/questions/comment-modifier-mes-coordonnees-bancaires-mon-rib-pour-payer-mes-impots#:~:text=Pour%20effectuer%20cette%20modification%2C%20rendez,%C3%A0%20jour%20vos%20coordonn%C3%A9es%20bancaires%20%C2%BB"
 
 }
 
@@ -61,11 +69,13 @@ impot_gouv.save!
 
 
 p"creating AMELI app"
+
+ameli.logo = "ameli.png"
 ameli = App.new
 ameli.nom = "Ameli"
 ameli.url_redirections = {
   addresse: "https://www.ameli.fr/assure/adresses-et-contacts/un-changement-de-situation/declarer-un-changement-de-coordonnees/declarer-une-nouvelle-adresse-postale" ,
- rib:"https://www.ameli.fr/assure/adresses-et-contacts/un-changement-de-situation/declarer-un-changement-de-coordonnees/declarer-un-changement-de-compte-bancaire"
+  rib:"https://www.ameli.fr/assure/adresses-et-contacts/un-changement-de-situation/declarer-un-changement-de-coordonnees/declarer-un-changement-de-compte-bancaire"
 }
 
 ameli.save!
@@ -74,6 +84,7 @@ ameli.save!
 
 p "creating France Travail app"
 
+france_travail.logo = "francetravail.png"
 france_travail = App.new
 france_travail.name = "France Travail"
 france_travail.url_redirections = {
@@ -86,6 +97,8 @@ france_travail.save!
 
 
 p "creating CAF app"
+
+caf.logo = "caf.png"
 caf = App.new
 caf.name = "CaF"
 caf.url_redirections = {
@@ -97,53 +110,63 @@ caf.save!
 
 
 
- p "creating La POSTE app"
- laposte = App.new
- laposte.name = "laPoste"
- laposte.url_redirections = {
+p "creating La POSTE app"
 
- addresse: "https://www.laposte.fr/demenager/comment-faire-changement-adresse"
- }
+laposte.logo = "laposte.jpg"
+laposte = App.new
+laposte.name = "laPoste"
+laposte.url_redirections = {
+  addresse: "https://www.laposte.fr/demenager/comment-faire-changement-adresse"
+}
 
- laposte.save!
+laposte.save!
 
  # Assurances et Mutelles
 
- p "creating generali app"
- generali = App.new
- generali.nom = "Generali"
- generali.url_redirections = {
- addresse: "https://www.generali.fr/agence/lecomte/aquitaine/assurance/je-demenage#:~:text=Avec%20Generali%2C%20profitez%20d'un%20d%C3%A9m%C3%A9nagement%20sans%20stress&text=Avec%20Generali%2C%20tout%20est%20simplifi%C3%A9,adapt%C3%A9%20%C3%A0%20votre%20nouvelle%20habitation" ,
- rib: "https://www.generali.fr/aide/user-question/modifie-mon-rib#:~:text=Je%20veux%20changer%20les%20coordonn%C3%A9es,choisir%20%3A%20Changement%20de%20coordonn%C3%A9es%20bancaires."}
+p "creating generali app"
+
+generali.logo = "generali.jpg"
+generali = App.new
+generali.nom = "Generali"
+generali.url_redirections = {
+addresse: "https://www.generali.fr/agence/lecomte/aquitaine/assurance/je-demenage#:~:text=Avec%20Generali%2C%20profitez%20d'un%20d%C3%A9m%C3%A9nagement%20sans%20stress&text=Avec%20Generali%2C%20tout%20est%20simplifi%C3%A9,adapt%C3%A9%20%C3%A0%20votre%20nouvelle%20habitation" ,
+rib: "https://www.generali.fr/aide/user-question/modifie-mon-rib#:~:text=Je%20veux%20changer%20les%20coordonn%C3%A9es,choisir%20%3A%20Changement%20de%20coordonn%C3%A9es%20bancaires."
+}
 
 generali.save!
 
 
- p "creating MACSF app"
-macsf = App.new
- macsf.nom = "MACSF"
- macsf.url_redirections = {
+p "creating MACSF app"
 
- addresse: "https://www.macsf.fr/vie-pratique/habitation/je-demenage-aide-au-demenagement" ,
- rib: ""
+macsf.logo = "macsf.jpg"
+macsf = App.new
+macsf.nom = "MACSF"
+macsf.url_redirections = {
+  addresse: "https://www.macsf.fr/vie-pratique/habitation/je-demenage-aide-au-demenagement" ,
+  rib: "https://www.macsf.fr/aide-et-contact/espace-personnel/questions-frequentes-espace-personnel#:~:text=Vous%20pouvez%20changer%20le%20compte,cliquez%20sur%20Changer%20de%20compte."
  }
 
- macsf.save!
+macsf.save!
 
- p "creating Swisslife app"
- swisslife = App.new
- swisslife.nom = "Allianz"
- swisslife.url_redirections = {
+
+p "creating Swisslife app"
+
+swisslife.logo = "swisslife.png"
+swisslife = App.new
+swisslife.nom = "Swisslife"
+swisslife.url_redirections = {
    addresse: "https://www.swisslife-select.ch/fr/decidez-vous-meme/evenements-delavie/umzug.html" ,
    rib: "https://myswisslife.fr/connection/login"
- }
+}
 
-   swisslife.save!
-
-
+swisslife.save!
 
 
- p "creating Allianz app"
+
+
+p "creating Allianz app"
+
+allianz.logo = "allianz.jpg"
 allianz = App.new
 allianz.nom = "Allianz"
 allianz.url_redirections = {
@@ -151,49 +174,60 @@ allianz.url_redirections = {
   rib: "https://www.allianz.fr/assurance-particulier/service-client/mon-allianz-mobile.html"
 }
 
-  allianz.save!
+allianz.save!
+
 
 
 # BANQUES
 
- p "creating Crédit Agricole app"
- credit_agricole = App.new
- credit_agricole.nom = "Credit_Agricole"
- credit_agricole.url_redirections = {
-addresse: "https://www.credit-agricole.fr/particulier/conseils/magazine/tout-un-mag/demenagement-les-10-reflexes-du-credit-agricole.html#:~:text=PR%C3%89VENIR%20SON%20BANQUIER&text=Ou%20vous%20%C3%AAtes%20d%C3%A9j%C3%A0%20client,ou%20de%20suivre%20ces%20d%C3%A9marches" ,
-rib: "https://www.credit-agricole.fr/particulier/conseils/magazine/tout-un-mag/comment-changer-de-banque-avec-la-loi-macron-c-est-facile.html"
+p "creating Crédit Agricole app"
+
+credit_agricole.logo = "creditagricole.jpg"
+credit_agricole = App.new
+credit_agricole.nom = "Credit_Agricole"
+credit_agricole.url_redirections = {
+ addresse: "https://www.credit-agricole.fr/particulier/conseils/magazine/tout-un-mag/demenagement-les-10-reflexes-du-credit-agricole.html#:~:text=PR%C3%89VENIR%20SON%20BANQUIER&text=Ou%20vous%20%C3%AAtes%20d%C3%A9j%C3%A0%20client,ou%20de%20suivre%20ces%20d%C3%A9marches" ,
+ rib: "https://www.credit-agricole.fr/particulier/conseils/magazine/tout-un-mag/comment-changer-de-banque-avec-la-loi-macron-c-est-facile.html"
  }
 
- credit_agricole.save!
+credit_agricole.save!
 
 
- p "creating  BNP app"
+
+p "creating  BNP app"
+
+bnp.logo = "bnp.jpg"
 bnp = App.new
 bnp.nom = "BNP"
 bnp.url_redirections = {
-addresse: "https://mabanque.bnpparibas/fr/gerer/comptes-et-formules/gerer-ses-comptes/demenager-formalites" ,
-rib: "https://mabanque.bnpparibas/fr/gerer/services-lies-compte/options-et-services/justificatifs-informations-personnelles"
+ addresse: "https://mabanque.bnpparibas/fr/gerer/comptes-et-formules/gerer-ses-comptes/demenager-formalites" ,
+ rib: "https://mabanque.bnpparibas/fr/gerer/services-lies-compte/options-et-services/justificatifs-informations-personnelles"
  }
 
- bnp.save!
+bnp.save!
 
 
- p "creating  HelloBank app"
+p "creating  HelloBank app"
+
+hellobank.logo = "hellobank.jpg"
 hellobank = App.new
 hellobank.nom = "HelloBank"
-bhellobank.url_redirections = {
-addresse:"https://www.hellobank.fr/faq/comment-modifier-mon-adresse-postale.html#:~:text=Reponse%3A,puis%20sur%20%22Mon%20adresse%22" ,
+hellobank.url_redirections = {
+ addresse:"https://www.hellobank.fr/faq/comment-modifier-mon-adresse-postale.html#:~:text=Reponse%3A,puis%20sur%20%22Mon%20adresse%22" ,
  rib: "https://www.hellobank.fr/lexique/changement-de-coordonnees-bancaires/#:~:text=Pour%20que%20le%20changement%20de,bancaire%20d%C3%A8s%20le%20mois%20suivant.&text=Les%20plus%20d'Hello%20bank!"
 }
 
 hellobank.save!
 
 
+
 p "creating  Societe Generale app"
+
+societe_generale.logo = "societegeneral.png"
 societe_generale = App.new
 societe_generale.nom = "Societe_generale"
 societe_generale.url_redirections = {
-addresse:"https://particuliers.sg.fr/nos-conseils/comptes-et-cartes/demenage-change-banque" ,
+ addresse:"https://particuliers.sg.fr/nos-conseils/comptes-et-cartes/demenage-change-banque" ,
  rib: "https://particuliers.sg.fr/services-en-ligne/mobilite-bancaire-service-bienvenue"
 }
 
@@ -201,97 +235,117 @@ societe_generale.save!
 
  # Telecommunication
 
- p "creating SFR app"
- sfr = App.new
- sfr.name = "SFR"
- sfr.url_redirections = {
+p "creating SFR app"
+
+sfr.logo = "sfr.jpg"
+sfr = App.new
+sfr.name = "SFR"
+sfr.url_redirections = {
   addresse: "https://www.sfr.fr/offre-internet/demenagement?esl-k=google%7Cng%7Cc443888133540%7Cmp%7Ckd%C3%A9m%C3%A9nagement%20sfr%7Cp%7Ct%7Cdc%7Ca105645464840%7Cg10371243279&gclsrc=aw.ds&sfrcpid=t1_google_fixe&utm_source=google&utm_medium=cpc&utm_campaign=m_fixe&utm_term=&utm_content=&gad_source=1&gclid=Cj0KCQjww5u2BhDeARIsALBuLnNiPci-kbJHzepewqPvkZaTG52W09zz3Hho8UpppHxHRHecti0EGYwaAs4JEALw_wcB&r=61670133" ,
   rib: "https://assistance.sfr.fr/gestion-client/offre-contrat/maj-coordonnees-bancaires-espace-client.html"
  }
 
- sfr.save!
+sfr.save!
 
 
- p "creating ORANGE app"
- orange = App.new
- orange.name = "Orange"
- orange.url_redirections = {
+p "creating ORANGE app"
+
+orange.logo = "orange.png"
+orange = App.new
+orange.name = "Orange"
+orange.url_redirections = {
   addresse: "https://www.orange.be/fr/produits-et-services/internet-tv-mobile/demenagement-love?utm_source=google&utm_medium=cpc&utm_campaign=b2c-act-fr-love-overview-alwayson-bra-movers&gad_source=1&gclid=Cj0KCQjww5u2BhDeARIsALBuLnMTi60Iv42i1gtsKfnYSrvCaQGvvLdX2kgKii0Lq-7DHBrQPxZQCjYaAofCEALw_wcB" ,
   rib: "https://orangeetmoi.orange.fr/"
   }
 
-  orange.save!
+orange.save!
 
 
-  p "creating FREE app"
- free = App.new
- free.nom = "Free"
- free.url_redirections = {
+p "creating FREE app"
+
+free.logo = "free.png"
+free = App.new
+free.nom = "Free"
+free.url_redirections = {
   addresse: "https://www.free.fr/freebox/demenagement/" ,
   rib: "https://assistance.free.fr/articles/904"
  }
-  free.save!
+free.save!
 
 
-  p "creating Bouygues Telecome app"
-  bouyguestelecome = App.new
-  bouyguestelecome.nom = "BouyguesTelecome"
-  bouyguestelecome.url_redirections = {
+
+p "creating Bouygues Telecome app"
+
+bouyguestelecome.logo = "bouyguestelecom.jpg"
+bouyguestelecome = App.new
+bouyguestelecome.nom = "BouyguesTelecome"
+bouyguestelecome.url_redirections = {
    addresse: "https://www.bouyguestelecom.fr/offres-internet/demenagement" ,
    rib: "https://www.assistance.bouyguestelecom.fr/s/article/coordonnees-bancaires-adresse-facturation"
   }
-   bouyguestelecome.save!
+bouyguestelecome.save!
 
 
 
-   p "creating SOSH app"
-  sosh = App.new
-  sosh.nom = "Sosh"
-  sosh.url_redirections = {
+  p "creating SOSH app"
+
+sosh.logo = "sosh.png"
+sosh = App.new
+sosh.nom = "Sosh"
+sosh.url_redirections = {
    addresse: "https://shop.sosh.fr/demenagement" ,
    rib: "https://communaute.sosh.fr/t5/Ma-facture-mon-paiement-Mon/Changement-de-compte-bancaire-pour-pr%C3%A9l%C3%A8vements/td-p/2357610"
   }
-   sosh.save!
+sosh.save!
 
    # DIVERTISSEMENTS
 
-   p "creating Disney app"
-   disney = App.new
-   disney.nom = "Disney"
-   disney.url_redirections = {
-    addresse: "https://www.disneyplus.com/identity/login/enter-email?pinned=true"
+p "creating Disney app"
 
-   }
-    disney.save!
-
-    p "creating Amazon app"
-    amazon = App.new
-    amazon.nom = "Amazon"
-    amazon.url_redirections = {
-     addresse: "https://www.amazon.fr/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.fr%2Fcompte%2Fs%3Fk%3Dvotre%2Bcompte%26ref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=frflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0"
-
-    }
-     amazon.save!
-
-
-     p "creating Spotify app"
-    spotify = App.new
-    spotify.nom = "Spotify"
-    spotify.url_redirections = {
-    addresse: "https://accounts.spotify.com/fr/login" ,
-    rib: "https://support.spotify.com/fr/article/update-payment-details/"
-
-   }
-    spotify.save!
+disney.logo = "disneyplus.png"
+disney = App.new
+disney.nom = "Disney"
+disney.url_redirections = {
+  addresse: "https://www.disneyplus.com/identity/login/enter-email?pinned=true"
+}
+disney.save!
 
 
 
-     p "creating Netflix app"
-    netflix = App.new
-    netflix.nom = "Netflix"
-    netflix.url_redirections = {
-    addresse: "https://www.netflix.com/login" ,
-    rib: "https://help.netflix.com/fr/node/244"
+p "creating Amazon app"
 
-   }
-    netflix.save!
+amazon.logo = "amazon.png"
+
+amazon = App.new
+mazon.nom = "Amazon"
+amazon.url_redirections = {
+  addresse: "https://www.amazon.fr/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.fr%2Fcompte%2Fs%3Fk%3Dvotre%2Bcompte%26ref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=frflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0"
+}
+amazon.save!
+
+
+p "creating Spotify app"
+
+spotify.logo = "spotifr.png"
+spotify = App.new
+spotify.nom = "Spotify"
+spotify.url_redirections = {
+ addresse: "https://accounts.spotify.com/fr/login" ,
+ rib: "https://support.spotify.com/fr/article/update-payment-details/"
+
+}
+spotify.save!
+
+
+
+p "creating Netflix app"
+
+netflix.logo = "netflix.jpg"
+netflix = App.new
+netflix.nom = "Netflix"
+netflix.url_redirections = {
+  addresse: "https://www.netflix.com/login" ,
+  rib: "https://help.netflix.com/fr/node/244"
+
+}
+netflix.save!
